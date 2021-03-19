d971bdf111095a15ccab68fe2bc7a3337e2d2c55
Thaila52 <thaila.grag@gmail.com>
2021-03-19 12:25:05 -0300

    Quadrado e ao cubo de um número dentro de uma lista

---
+++ src/Quadrado_e_ao_Cubo.kt
@@ -0,0 +1,8 @@
+fun main(args: Array<String>) {
+    //insira as variaveis corretamente
+    var r = 0
+    for (i in 1..readLine()!!.toInt()) {
+        println("$i ${ i *  i } ${ i * i * i }")
+        r += 1
+    }
+}
